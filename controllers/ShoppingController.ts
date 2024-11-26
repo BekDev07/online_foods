2e8b06e65d1d092b9211e56adc961be3369dd0a2
BekDev07 <elbekmovlonov@gmail.com>
2024-11-26 18:53:59 +0500

    added some controllers

--- controllers/ShoppingController.ts
+++ controllers/ShoppingController.ts
@@ -1,5 +1,6 @@
 import { Request, Response, NextFunction } from "express-serve-static-core";
 import { Vendor } from "../models";
+import { FoodDoc } from "../models/Food";
 
 export const GetFoodAvailability = async (req: Request, res: Response, next: NextFunction) => {
   const pincode = req.params.pincode;
@@ -15,6 +16,7 @@ export const GetFoodAvailability = async (req: Request, res: Response, next: Nex
 }
 
 export const GetTopRestaurants = async (req: Request, res: Response, next: NextFunction) => {
+
   const pincode = req.params.pincode;
 
   const result = await Vendor.find({ pincode: pincode, serviceAvailable: true }).sort([['rating', 'descending']]).limit(10)
@@ -30,13 +32,55 @@ export const GetTopRestaurants = async (req: Request, res: Response, next: NextF
 
 export const GetFoodsIn30Min = async (req: Request, res: Response, next: NextFunction) => {
 
+  const pincode = req.params.pincode;
+
+  const result = await Vendor.find({ pincode: pincode, serviceAvailable: true }).sort([['rating', 'descending']]).populate('foods');
+
+  if (result.length > 0) {
+    let foodResult: any = [];
+    result.map(vendor => {
+      const foods = vendor.foods as [FoodDoc];
+      foodResult.push(...foods.filter(food => food.readyTime <= 30));
+    })
+    res.status(200).json(foodResult);
+    return
+  }
+
+  res.status(404).json({ msg: 'data Not found!' });
+  return
 }
+
 export const SearchFoods = async (req: Request, res: Response, next: NextFunction) => {
 
-}
-export const GetAvailableOffers = async (req: Request, res: Response, next: NextFunction) => {
+  const pincode = req.params.pincode;
+  const result = await Vendor.find({ pincode: pincode, serviceAvailable: true })
+    .populate('foods');
 
+  if (result.length > 0) {
+    let foodResult: any = [];
+    result.map(item => foodResult.push(...item.foods));
+    res.status(200).json(foodResult);
+    return
+  }
+  res.status(404).json({ msg: 'data Not found!' });
+  return
 }
+
 export const RestaurantById = async (req: Request, res: Response, next: NextFunction) => {
+
+  const id = req.params.id;
+
+  const result = await Vendor.findById(id).populate('foods')
+
+  if (result) {
+    res.status(200).json(result);
+    return
+  }
+
+  res.status(404).json({ msg: 'data Not found!' });
+  return
+}
+
+export const GetAvailableOffers = async (req: Request, res: Response, next: NextFunction) => {
 
 }
